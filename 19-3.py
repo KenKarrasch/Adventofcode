95c241845acaf59d4958fd91b5a5846309774928
Ken Karrasch <32858778+KenKarrasch@users.noreply.github.com>
2019-12-04 08:06:56 +1000

    Update 19-3.py

--- 19-3.py
+++ 19-3.py
@@ -1,5 +1,5 @@
 f = open('19-3.txt').read().split('\n')
-# tried using a grid, ran out of memory. Tried calculating analytically, didn't work for all cases. 
+# tried using a grid, ran out of memory. Tried calculating to each corner and then finding crossovers analytically, didn't work for all cases. 
 #Ended up using sets, storing each step in memory.
 u = [f[0].split(',')]
 u.append(f[1].split(','))
@@ -7,9 +7,7 @@ u.append(f[1].split(','))
 w = [[1,0],[-1,0],[0,-1],[0,1]]#rlud
     
 def gp(l):
-    x,y = 0,0
-    path = {}
-    wd = 0
+    u,v,path,wd = 0,0,{},0
     for d in l:
       p = [0,0]
       if 'R' in d:
@@ -21,23 +19,20 @@ def gp(l):
       if 'D' in d:
        p = w[3]
       for _ in range(int(d[1:])):
-        x += p[0]
-        y += p[1]
         wd += 1
-        if (x,y) not in path:
-          path[(x,y)] = wd
+        u += p[0]
+        v += p[1]
+        path[(u,v)] = wd
     return path
 
-sa = gp(u[0])
-sb = gp(u[1])
-btb = set(sa.keys())&set(sb.keys())
+s1,s2 = gp(u[0]),gp(u[1])
+btb = set(s1.keys())&set(s2.keys())
 
-lst = []
-lwd = []
-for (x,y) in btb:
-   lst.append(abs(x)+abs(y))
+lst, lwd = [],[]
+for (u,v) in btb:
+   lst.append(abs(u)+abs(v))
 for ind in btb:
-   lwd.append(sa[ind]+sb[ind])
+   lwd.append(s1[ind]+s2[ind])
    
 print 'part 1 -',min(lst)
 print 'part 2 -',min(lwd)
