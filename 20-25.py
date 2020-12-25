e65e16c11ee6fd453710f20f19317ea823c3d7f9
Ken Karrasch <32858778+KenKarrasch@users.noreply.github.com>
2020-12-26 09:21:04 +1000

    Update 20-25.py

--- 20-25.py
+++ 20-25.py
@@ -1,10 +1,8 @@
-# Christmas Encryption Puzzle
+# Christmas Encryption Puzzle 
+# (originally forgot about the inbuilt pow function)
 k1,k2 = [int(x) for x in open('20-25.txt').read().split('\n')]
 lp,n = 1,1
 while True:
     n,lp = (n*7)%20201227,lp+1
     if n == k2: break    
-n = 1
-for i in range(lp-1):
-    n = (n*k1)%20201227
-print('part 1 -', n)
+print('part 1 -',pow(k1,lp-1,20201227))
