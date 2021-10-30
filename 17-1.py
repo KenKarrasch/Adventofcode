516161bfbeb6ed3d65593efe6fba511d713c65a2
Ken Karrasch <32858778+KenKarrasch@users.noreply.github.com>
2021-10-30 14:30:41 +1000

    Create 17-1.py

---
+++ 17-1.py
@@ -0,0 +1,10 @@
+f = [int(i) for i in open('17-1.txt').read()]
+t1,t2 = 0,0
+for i in range(len(f)):
+    if f[i-1] == f[i]:
+        t1 += f[i]    
+l = len(f)
+for i in range(l):        
+    if f[i] == f[(i + int((l+1)/2))%l]:
+        t2 += f[i]
+print('part 1 - ', t1, ', part 2 - ',t2)
