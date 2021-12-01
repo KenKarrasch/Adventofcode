f035932669c2b8cfff045af91ea45ed304da119a
Ken Karrasch <32858778+KenKarrasch@users.noreply.github.com>
2021-12-01 18:05:52 +1000

    Create 21-1.py

---
+++ 21-1.py
@@ -0,0 +1,13 @@
+r = [int(i) for i in open('te').read().split()]
+ct = p = 0
+
+for i in r:
+    if i > p: ct += 1
+    p = i
+print 'part 1 -',ct-1
+ct = p = 0
+for i in range(len(r)-2):
+    i = sum(r[i:i+3])
+    if i > p: ct += 1
+    p = i
+print 'part 2 -',ct
