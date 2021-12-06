979d8fb4061c774eec7f750b1a5cdcff09542e77
Ken Karrasch <32858778+KenKarrasch@users.noreply.github.com>
2021-12-06 22:12:41 +1000

    Update 21-5.py

--- 2021/21-5.py
+++ 2021/21-5.py
@@ -13,17 +13,17 @@ for pt in [1,2]:
   for i in cs:
     if i[0][1] == i[1][1]:                
             for r in range(1+abs(i[0][0] - i[1][0])):                        
-                if i[0][0] < i[1][0]: ad(i[0][0]+r + (1000*i[1][1]))  
-                else: ad(i[1][0]+r + (1000*i[1][1]))                             
+                if i[0][0] < i[1][0]: ad((i[0][0]+r,i[1][1]))  
+                else: ad((i[1][0]+r,i[1][1]))                             
     elif i[0][0] == i[1][0]:            
             for r in range(1+abs(i[0][1] - i[1][1])):
-                if i[0][1] < i[1][1]: ad(i[0][0] + (1000*(i[0][1]+r)))             
-                else: ad(i[1][0] + (1000*(i[1][1]+r)))           
+                if i[0][1] < i[1][1]: ad((i[0][0],i[0][1]+r))            
+                else: ad((i[1][0],i[1][1]+r))           
     else:  
         if pt == 2:       
             dx,dy = i[1][0] - i[0][0],i[1][1] - i[0][1]                    
             b = i[0][:] if dx > 0 else i[1][:]
             for r in range(abs(i[0][0] - i[1][0])+1):                           
-                if dx*dy > 0: ad(b[0]+r + (1000*(b[1]+r)))                              
-                else: ad(b[0]+r + (1000*(b[1]-r)))                             
-  print('part',pt,'-',sum([1 for i in list(gd.values()) if i > 1]))  
+                if dx*dy > 0: ad((b[0]+r,b[1]+r))                              
+                else: ad((b[0]+r,b[1]-r))                             
+  print('part',pt,'-',sum([1 for i in list(gd.values()) if i > 1]))
