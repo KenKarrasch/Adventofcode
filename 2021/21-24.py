b47034fb53d6df93ba975902ff8f17f0401070d6
Ken Karrasch <32858778+KenKarrasch@users.noreply.github.com>
2022-01-04 11:53:12 +1000

    Update 21-24.py

--- 2021/21-24.py
+++ 2021/21-24.py
@@ -13,7 +13,6 @@ dn = False
 sc = [False]
 v1 = 18  
 
-
 def check(n):
  ni = n 
  cmp = [] 
@@ -31,18 +30,19 @@ def check(n):
    v['z'] = 0
    p = 0     
    bk = [] 
-   cyc = 18  # for now only do one cycle
+   cyc = 18  
    #print('--------------------',ver)
    sc[0] = True
    if ver == v1:
-    for c in range(14):   
+    for c in range(14):  # Simplified ALU code
      d1 = int(s[4+cyc*c].split()[2])
      a1 = int(s[5+cyc*c].split()[2])
      a2 = int(s[15+cyc*c].split()[2])   
      z = v['z']  
      if d[0]==(z%26)+a1: x = 0 
      else: x = 1               
-     if d1 == 26 and x == 1: # If statment failed, save how far we got, sc[0]
+     if d1 == 26 and x == 1: 
+        # If statment failed, save how far we got, in memory sc[0]
         print(ni,'fail')  
         sc[0] = c
         return False
@@ -64,7 +64,8 @@ def check(n):
      for k in range(st): bk.append([0])     
    pr = p
 
-   while (p < len(s)):
+   while (p < len(s)): 
+    # Original ALU code left in not actually needed after simplification above
     i = s[p].split()        
     if i[0] == 'inp':       
       v[i[1]] = d[0]      
@@ -95,7 +96,9 @@ def check(n):
       if cmp[0][i] == cmp[1][i]:
         gds += 1
       else:
-        diff += 1 # Debugging code to ensure simplifications worked
+        diff += 1 
+   # Debugging code to compare simplified code with raw ALU to 
+   # ensure simplifications actually worked
  print(ni,v)
  if v['z'] == 0:
     return True
@@ -127,25 +130,30 @@ print('part 2 -',pt2)
 
 # The assembly language needed to be boiled down quite a bit to understand
 # what was going on.  The 252 lines of code appeared to be the same 18 lines
-# repeated 14 times, with only three variables changing.  'div z 1' (d1), 
-# 'add x 10' (a1), and 'add y 5' (a2).
+# repeated 14 times, with only three variables changing. line 5: 'div z d1' 
+# (d1 = 1 or 26), line 6: 'add x a1' (a1 = number), and line 16:
+# 'add y a2' (a2 = number).
 # 
 # My code came down to this, 18 times:
 #
-# w = d[0]
-# if d[0]==z%26+a1: x = 0 else: x = 1
+# if d[N]==z%26+a1: x = 0 else: x = 1
 # z = z//d1
-# z = z*(25*x+1) + (d[0]+a2)*x
+# z = z*(25*x+1) + (d[N]+a2)*x
 #
-# The key was to get the if statement to be true whenever 'div z 1' appeared
-# and false when 'div z 26' occurred.
+# To get the z value down to zero the key was for each of the 18 iterations to 
+# get the if statement to be true whenever 'div z 1' appeared and false when
+# 'div z 26' appeared.
 #
 # In order to acheive this start at the highest digit, 99999999999999, 
-# and see how many if statements it gets through, then decrement one,
-# 8999999999999, until the if statement gets further. When this happens move onto the
-# next digit, until all digits are solved.  Pretty sure this problem 
-# could be solved by hand.  My answer was 99919692496939.
+# and see how many if statements it gets through, if it does not get through the first one
+# decrement one, to 8999999999999, when it gets through the first if statement move onto
+# the next digit then decrement that, say to 8899999999999 then 8799999999999 etc,
+# until the if statement gets further. Keep doing this until all if statements are correct
+# and digitis are solved.  Pretty sure this problem could probably be solved by hand
+# without a calculator.  My answer was 99919692496939, the first three digits were
+# nine so the first scan made it to the fourth if statement straight out of the gate, which
+# means a brute force solve may actually have been feasible.
 #
 # For part two do the opposite, start at 11111111111111, and increment.
 #
-#  Debugging code left in
+# Debugging code left in
