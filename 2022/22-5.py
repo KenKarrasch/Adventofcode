3accbbe7c1ac9f962ecef1e1b5438bd06a186345
Ken Karrasch <32858778+KenKarrasch@users.noreply.github.com>
2022-12-05 21:27:14 +1000

    Update 22-5.py

--- 2022/22-5.py
+++ 2022/22-5.py
@@ -2,18 +2,15 @@ import re
 from copy import deepcopy
 f = open('22-5.txt').read().split('\n')
 b = [[] for i in range(9)]
-for i in f:        
-    if '[' in i:            
-      for j in range(9):    
-        ch = 1 + j*4
-        if(ch < len(i)):
-            if(i[ch] != ' '):                
-                b[j].append(i[ch])
+for i in [x for x in f if '[' in x]:      
+  for j in range(9):    
+    if(1+j*4 < len(i)):
+      if(i[1+j*4] != ' '):                
+        b[j].append(i[1+j*4])
 for (pt,d) in [(1,-1),(2,1)]:
   g = deepcopy(b)
-  for i in f:   
-    if 'move' in i:
-        sz,s,e = [int(k)-1 for k in re.findall(r'\d+', i)]                        
-        g[e] = g[s][:sz+1][::d] + g[e]                
-        g[s] = g[s][sz+1:]        
+  for i in [x for x in f if 'to' in x]: 
+    sz,s,e = [int(k)-1 for k in re.findall(r'\d+', i)]                        
+    g[e] = g[s][:sz+1][::d] + g[e]                
+    g[s] = g[s][sz+1:]        
   print('part',pt,'-',''.join(k[0] for k in g))
