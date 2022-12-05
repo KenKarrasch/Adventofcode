9eb6cfed1aa65e07343d1686b56141ca07db7231
Ken Karrasch <32858778+KenKarrasch@users.noreply.github.com>
2022-12-05 21:46:22 +1000

    Update 22-5.py

--- 2022/22-5.py
+++ 2022/22-5.py
@@ -1,16 +1,12 @@
 import re
-from copy import deepcopy
 f = open('22-5.txt').read().split('\n')
 b = [[] for i in range(9)]
-for i in [x for x in f if '[' in x]:      
-  for j in range(9):    
-    if(1+j*4 < len(i)):
-      if(i[1+j*4] != ' '):                
-        b[j].append(i[1+j*4])
+for i in [x for x in f if '[' in x]:       
+  for j in range(9):
+      if(i[1+j*4] != ' '): b[j].append(i[1+j*4])
 for (pt,d) in [(1,-1),(2,1)]:
-  g = deepcopy(b)
+  g = [x for x in b[::]]
   for i in [x for x in f if 'to' in x]: 
-    sz,s,e = [int(k)-1 for k in re.findall(r'\d+', i)]                        
-    g[e] = g[s][:sz+1][::d] + g[e]                
-    g[s] = g[s][sz+1:]        
+    sz,s,e = [int(k)-1 for k in re.findall(r'\d+', i)]
+    g[e],g[s] = g[s][:sz+1][::d]+g[e],g[s][sz+1:]
   print('part',pt,'-',''.join(k[0] for k in g))
