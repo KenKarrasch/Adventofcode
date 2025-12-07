5e469415bef0c26dd2a1c28461e042e7d375d639
Ken Karrasch <32858778+KenKarrasch@users.noreply.github.com>
2025-12-07 16:36:41 +1000

    Create 7-2.py

---
+++ 2025/7-2.py
@@ -0,0 +1,32 @@
+f = open('25-7.txt').read().split('\n')
+
+# Another pretty good rank score
+# 7     174    502  **  pt1 
+# 7    5170   2713  ***  pt2
+
+tb = []
+tbc = {}
+
+DP = {}
+tly = 0
+for r in range(len(f)):    
+    for c in range(len(f[r])):
+        if f[r][c] == '^':
+            if c in tb:
+                tly += 1     
+                for d in [-1,1]:       
+                    if c+d not in tb:                                                                    
+                        tb.append(c+d)   
+                        tbc[c+d] = tbc[c] 
+                    else:
+                        tbc[c+d] += tbc[c]                    
+                tb.remove(c)   
+                tbc[c] = 0                          
+        if f[r][c] == 'S':            
+            tb.append(c)
+            tbc[c] = 1
+        
+p = 0
+for i in tbc.items():    
+    p += i[1]
+print(p)
