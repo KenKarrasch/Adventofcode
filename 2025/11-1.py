f8ea662b041f0b3069097b49bfbc6973c2319296
Ken Karrasch <32858778+KenKarrasch@users.noreply.github.com>
2025-12-11 15:40:32 +1000

    Create 11-1.py

---
+++ 2025/11-1.py
@@ -0,0 +1,25 @@
+f = open('in.txt').read().split('\n')
+
+rl = {}
+for r in f:
+    st = r.split(':')[0]
+    es = r.split(':')[1].split()
+    rl[st] = es
+
+Q = ['you']
+
+print(rl.items())
+
+tly = 0
+SEEN = []
+
+while Q:
+    pl = Q.pop()
+    if pl == 'out':
+        tly += 1
+        continue
+    if pl in rl.keys():
+        for i in rl[pl]:
+            Q.append(i)
+
+print(tly)
